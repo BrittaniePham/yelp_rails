09d7ca7acf0c0e039b86528bf303dd5b71e2196f
Brittanie Pham <brittaniepham@gmail.com>
2018-05-25 14:45:07 -0600

    index with dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  Restaurant.create(
+    title: Faker::HowIMetYourMother.catch_phrase,
+    author: Faker::HowIMetYourMother.character,
+    body: Faker::HowIMetYourMother.quote,
+  )
+end
+
+puts 'seeded'
